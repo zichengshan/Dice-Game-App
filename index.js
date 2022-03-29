9796df025a4c42832acb661d1f3ee0b72d75f986
Zicheng.S <zichengshan@gmail.com>
2022-03-28 20:53:02 -0700

    set up the DOM nodes

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 
+// Create variables for the game state
 let player1Score = 0
 let player2Score = 0
 let player1Turn = true
+
+// Create variables to store references to the necessary DOM nodes
+const player1DiceEl = document.getElementById("player1Dice")
+const player2DiceEl = document.getElementById("player2Dice")
+const player1ScoreboardEL = document.getElementById("player1Scoreboard")
+const player2ScoreboardEL = document.getElementById("player2Scoreboard")
+const messageEl = document.getElementById("message")
+const rollBtnEl = document.getElementById("rollBtn")
+const resetBtnEl = document.getElementById("resetBtn")
+
+
